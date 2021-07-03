77af0a0d6d725cc3a27e698010a1cc267e0d9bde
Epinse <82251000+Epinse@users.noreply.github.com>
2021-07-03 11:03:16 +0700

    Update Koneksi.java

--- src/Program/Koneksi.java
+++ src/Program/Koneksi.java
@@ -4,30 +4,23 @@
  * and open the template in the editor.
  */
 package Program;
+import com.mysql.jdbc.Driver;
+import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import javax.swing.JOptionPane;
 /**
  *
  * @author ASUS
  */
 public class Koneksi {
-    private static Connection mysqlkonek;
-    public static Connection koneksiDB()throws SQLException{
-        if(mysqlkonek==null){
-        try{
-        String DB ="jdbc:mysql://localhost/tubes_pbo";
-	String user ="root";
-	String pass="";
-        DriverManager.registerDriver(new com.mysql.jdbc.Driver());
-	mysqlkonek = (Connection)DriverManager.getConnection(DB,user,pass);
-        } catch (Exception e){
-            JOptionPane.showMessageDialog(null," gagal koneksi");
-        }    
-        
+   private static Connection koneksi;
+    public static Connection GetConnection()throws SQLException{
+        if (koneksi==null){
+            new Driver();
+            koneksi = DriverManager.getConnection("jdbc:mysql://localhost:3306/pkn","root","");
         }
-        return mysqlkonek;
+        return koneksi;
     }
 
     
@@ -49,3 +42,4 @@ try {
     return connect;
   }*/
 }
+
